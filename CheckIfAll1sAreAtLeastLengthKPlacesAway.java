e9a52f40bcbb5680ae3a7b4d8b0ccf133ecaba4d
Puruchandra <puruchandra4@gmail.com>
2021-01-26 23:18:50 +0530

    Update CheckIfAll1sAreAtLeastLengthKPlacesAway.java

--- CheckIfAll1sAreAtLeastLengthKPlacesAway.java
+++ CheckIfAll1sAreAtLeastLengthKPlacesAway.java
@@ -1,3 +1,5 @@
+//LINK - https://leetcode.com/problems/check-if-all-1s-are-at-least-length-k-places-away/
+
 class Solution {
     public boolean kLengthApart(int[] nums, int k) {
         int     cnt = k;
