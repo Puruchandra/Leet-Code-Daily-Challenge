6bec0a6778c24dd9ce77c23c274162073a672ed3
Puruchandra <puruchandra4@gmail.com>
2021-02-03 11:04:52 +0530

    Create MergeSortedArraysSTRV.java
    Merge Sorted Arrays STRV

---
+++ MergeSortedArraysSTRV.java
@@ -0,0 +1,47 @@
+    class Solution {
+
+        /*
+
+            Define one pointers in each array i, j at 0th index
+            compare the value of both pointers
+            if i <= j, the value at i is already at right place
+                move i, and hold j
+            else 
+                insert the value of pointer j in array1
+                move i and j
+            repeat until i and j reaches end
+
+
+        */
+        
+        
+        /*
+            Another approach
+            replace the array items in nums1 after pos m
+            sort the array
+            
+            
+        */
+
+
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+           int counter = 0;
+        for(int i=0; i<m+n && n>0 ; i++){
+            
+            if(i<m){
+                continue;
+            }else{
+                nums1[i] = nums2[counter];
+                counter++;
+            }
+            
+        
+        }
+            
+        
+                     
+        Arrays.sort(nums1);
+        }
+
+    }
