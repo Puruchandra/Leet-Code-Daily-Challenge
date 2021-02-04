1875f2d7a4cee3a9f16a0a1a0a7359f82e00dcac
Puruchandra <puruchandra4@gmail.com>
2021-02-05 00:50:52 +0530

    Update MergeIntervalsSTRV.java
    MergeIntervalsSTRV [done]

--- MergeIntervalsSTRV.java
+++ MergeIntervalsSTRV.java
@@ -1,14 +1,13 @@
 //LINK - https://leetcode.com/problems/merge-intervals
 
+
 class Solution {
     
     /*  
-        initialize a currMergableArray with intervals[0]
-        run a loop from 1 to intervals.length
-        check if the array at currMergableArray overlaps array at i
-            if true merge, update currMergableArray with newly merged
-            else update currMergableArray with array at i
-                
+        
+        sorting + linear time 
+        tc -> O(nlogn)
+        
     
     */
     
@@ -16,58 +15,114 @@ class Solution {
     public int[][] merge(int[][] intervals) {
     
         
-        Arrays.sort(intervals, new java.util.Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                    return Integer.compare(a[0], b[0]);
-             }});
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
         
-        ArrayList<int[]> list = new ArrayList<>();
+        LinkedList<int[]> merged = new LinkedList<>();
         
-        int[] currMergableArray = intervals[0];
             
-        if(intervals.length <= 1){
-            return intervals;
-        }
+       
         
-        for(int i=1; i<intervals.length; i++){
+        for(int[] interval : intervals){
             
-            if(isMergable(currMergableArray, intervals[i])){
-                
-                int[] merged = new int[]{ currMergableArray[0], Math.max(intervals[i][1], currMergableArray[1]) };
-                currMergableArray = merged;
-                list.add(currMergableArray);
-                
+            if(merged.isEmpty() || merged.getLast()[1] < interval[0]){
+                merged.add(interval);
             }else{
-                if(i == 1){
-                    list.add(currMergableArray);    
-                }
-                list.add(intervals[i]);
-                currMergableArray =  intervals[i];
+                merged.getLast()[1] = Math.max(merged.getLast()[1],interval[1]);
             }
-            
-            
         }
         
-        
-        // 
-        int[][] res = new int[list.size()][2]; 
-        int i = 0;
-        for(int[] arr : list){
-            res[i] = arr;
-            i++;
-        }
-        return res;
+        return merged.toArray(new int[merged.size()][]);
         
     }
     
-    
+    //not using this anymore
     public static boolean isMergable(int[] array1, int[] array2){
         
         if(array2[0] <= array1[1]){
-            
+            // System.out.println("ere");
             return true;
         }
         return false;
         
     }
+    
+    
+    
 }
+
+
+
+// not running all testcases
+
+
+// class Solution {
+    
+//     /*  
+//         initialize a currMergableArray with intervals[0]
+//         run a loop from 1 to intervals.length
+//         check if the array at currMergableArray overlaps array at i
+//             if true merge, update currMergableArray with newly merged
+//             else update currMergableArray with array at i
+                
+    
+//     */
+    
+    
+//     public int[][] merge(int[][] intervals) {
+    
+        
+//         Arrays.sort(intervals, new java.util.Comparator<int[]>() {
+//             public int compare(int[] a, int[] b) {
+//                     return Integer.compare(a[0], b[0]);
+//              }});
+        
+//         ArrayList<int[]> list = new ArrayList<>();
+        
+//         int[] currMergableArray = intervals[0];
+            
+//         if(intervals.length <= 1){
+//             return intervals;
+//         }
+        
+//         for(int i=1; i<intervals.length; i++){
+            
+//             if(isMergable(currMergableArray, intervals[i])){
+                
+//                 int[] merged = new int[]{ currMergableArray[0], Math.max(intervals[i][1], currMergableArray[1]) };
+//                 currMergableArray = merged;
+//                 list.add(currMergableArray);
+                
+//             }else{
+//                 if(i == 1){
+//                     list.add(currMergableArray);    
+//                 }
+//                 list.add(intervals[i]);
+//                 currMergableArray =  intervals[i];
+//             }
+            
+            
+//         }
+        
+        
+//         // 
+//         int[][] res = new int[list.size()][2]; 
+//         int i = 0;
+//         for(int[] arr : list){
+//             res[i] = arr;
+//             i++;
+//         }
+//         return res;
+        
+//     }
+    
+    
+//     public static boolean isMergable(int[] array1, int[] array2){
+        
+//         if(array2[0] <= array1[1]){
+            
+//             return true;
+//         }
+//         return false;
+        
+//     }
+// }
