15773309ff534402d6c61a3e6f20b357c53be2b0
Puruchandra <puruchandra4@gmail.com>
2021-02-07 22:56:41 +0530

    Max Profit STRV

---
+++ MaxProfitSTRV.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+    }
+}
+
+/*  
+    Brute Force Solution
+    T.C - 0(n^2) - Would be TLE for big numbers
+      
+    class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int maxP = 0;
+        
+        for(int i = 0; i < prices.length; i++){
+            for(int j = i ; j < prices.length; j++){
+                
+                if(i != j){
+                    
+                
+                int lsum = prices[j] - prices[i];
+                
+                maxP = Math.max(lsum, maxP);
+                }
+            }
+        }
+        
+        
+        return maxP;
+    }
+}
+
+*/
