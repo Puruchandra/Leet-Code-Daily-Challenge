7dd215a3142c61114be255ff66e9f1c58281441e
Puruchandra <puruchandra4@gmail.com>
2021-03-03 23:04:22 +0530

    Create AddToArray.java

---
+++ AddToArray.java
@@ -0,0 +1,24 @@
+LINK -- https://leetcode.com/problems/add-to-array-form-of-integer/submissions/
+
+class Solution {
+    
+    
+    
+    public List<Integer> addToArrayForm(int[] A, int K) {
+        List<Integer> res = new ArrayList<Integer>();
+        
+        int n = A.length;
+        int carry = K;
+        
+        while(--n >= 0 || carry > 0){
+            if (n >= 0)
+                carry+= A[n];
+            res.add(carry % 10);
+            carry /= 10;
+            
+        }
+        
+        Collections.reverse(res);
+        return res;
+    }
+}
