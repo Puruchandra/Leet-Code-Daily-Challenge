5c3d04a30d8e81b21a77506c2529cabc0602289a
Puruchandra <puruchandra4@gmail.com>
2021-03-19 23:30:01 +0530

    Update AddToArray.java

--- AddToArray.java
+++ AddToArray.java
@@ -1,4 +1,4 @@
-LINK --- https://leetcode.com/problems/add-to-array-form-of-integer/submissions/
+LINK -- https://leetcode.com/problems/add-to-array-form-of-integer/submissions/
 
 class Solution {
     
