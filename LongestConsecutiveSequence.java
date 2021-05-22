93bb67acb6ddcb1af633234a90571db11618cb08
Puruchandra <puruchandra4@gmail.com>
2021-05-22 23:36:04 +0530

    Update LongestConsecutiveSequence.java

--- LongestConsecutiveSequence.java
+++ LongestConsecutiveSequence.java
@@ -1,4 +1,4 @@
-// LINK -- https://leetcode.com/problems/longest-consecutive-sequence/
+//  LINK -- https://leetcode.com/problems/longest-consecutive-sequence/
 
 class Solution {
     public int longestConsecutive(int[] nums) {
