752c1e6c1a5fa587556353126b0c15b6e0e586e0
Puruchandra <puruchandra4@gmail.com>
2021-05-23 00:21:45 +0530

    Create Longest SubstringWithoutRepeatingCharacters.java
    hashing problem

---
+++ Longest SubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,19 @@
+//LINK -- https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> seen = new HashMap<>();
+        int start = 0;
+        int maxLen = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            char currChar = s.charAt(i);
+            if(seen.containsKey(currChar)) {
+                start = Math.max(seen.get(currChar) + 1, start);
+            }
+            seen.put(currChar, i);
+            maxLen = Math.max(i - start + 1, maxLen);
+        } 
+        return maxLen;  
+    }
+} 
